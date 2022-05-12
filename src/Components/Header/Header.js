120343c82adbaef920a201bf397858801fc009ca
TolomushFSDev <tolomushfs@gmail.com>
2022-05-12 16:31:13 +0600

    initial

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss'
+const Header = () => {
+    return (
+        <div>
+            <h1 className="title">Food store</h1>
+        </div>
+    );
+};
+
+export default Header;
